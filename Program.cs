efcccd816cb0c8babe5d531025ebbb0ec9ea4989
udaceue <rjemik9@gmail.com>
2023-11-24 13:40:43 +0100

    Initial commit

---
+++ Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Linq;
+
+namespace Program
+{
+    public static class Program
+    {
+        // Functions
+        public static void CarPrinter(string[] args)
+        {
+            Console.WriteLine("Hello World"); 
+        }
+    }
+}
